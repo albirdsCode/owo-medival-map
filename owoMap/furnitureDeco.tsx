b65d930b20f6a51bcd2d9b3b522dbc95c8912013
jonah <kikojonahalbert@gmail.com>
2026-03-13 13:51:19 +0100

    added new map version

---
+++ owoMap/furnitureDeco.tsx
@@ -0,0 +1,49 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="furnitureDeco" tilewidth="130" tileheight="97" tilecount="15" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="2c99d9e1-2328-4d35-934b-1b06def9b400-chair1.png" width="18" height="30"/>
+ </tile>
+ <tile id="1">
+  <image source="2e666ee1-6ef4-428f-aad7-871cb9d91b1e-plant3.png" width="47" height="60"/>
+ </tile>
+ <tile id="2">
+  <image source="4a34c33b-98d7-48d7-8baf-016a3d32b52e-little-table.png" width="32" height="90"/>
+ </tile>
+ <tile id="3">
+  <image source="4fc5896e-f31e-479f-aa4d-b1c153bf173d-table3.png" width="126" height="97"/>
+ </tile>
+ <tile id="4">
+  <image source="5aace06a-9143-4be0-b808-b7724344e18e-chair-blue3.png" width="22" height="33"/>
+ </tile>
+ <tile id="5">
+  <image source="7c6ca6a9-0c07-4735-93d4-a2f5b99948ce-table2.png" width="93" height="73"/>
+ </tile>
+ <tile id="6">
+  <image source="22a7a851-b08c-491c-a5f6-d81a1f5cb00f-chair-blue2.png" width="14" height="32"/>
+ </tile>
+ <tile id="7">
+  <image source="39e95f28-6246-4782-9c25-a61717d6b765-chair2.png" width="15" height="33"/>
+ </tile>
+ <tile id="8">
+  <image source="0480afa9-efbd-4a07-9b33-4d29e1241072-chair3.png" width="22" height="33"/>
+ </tile>
+ <tile id="9">
+  <image source="536f7224-05c3-4d1e-bbb8-38dbe570ca87-mini-table.png" width="63" height="67"/>
+ </tile>
+ <tile id="10">
+  <image source="910d0568-4b26-4fa2-85e3-d2e1bab3c2c7-chair-blue4.png" width="19" height="28"/>
+ </tile>
+ <tile id="11">
+  <image source="8451a29c-ccb0-4bad-8b28-875ca34c2f67-mini-bench.png" width="30" height="70"/>
+ </tile>
+ <tile id="12">
+  <image source="6495272c-d338-4464-bc81-3a55cf8aedb7-long-bench.png" width="130" height="30"/>
+ </tile>
+ <tile id="13">
+  <image source="a4f92f71-b5bd-4521-a096-27624b115aa7-chair-blue1.png" width="19" height="31"/>
+ </tile>
+ <tile id="14">
+  <image source="b4ec459d-95dc-48f2-9bcf-0ef6b84f4427-chair-blue4.png" width="19" height="28"/>
+ </tile>
+</tileset>
---
+++ owoMap/tudaLinks.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="tudaLinks" tilewidth="125" tileheight="83" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="d120-logo-16bit.png" width="125" height="83"/>
+ </tile>
+</tileset>
